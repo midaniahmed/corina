570b931de90c51928b6b7ff60507647a04095ff7
Midani Ahmed <mideni.ahmed@gmail.com>
2020-04-03 22:43:01 +0100

    fix favico

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: process.env.API_URL });
+// const api = axios.create({ baseURL: process.env.API_URL });
+const api = axios.create({ baseURL: "https://corona.lmao.ninja" });
 
 api.interceptors.request.use(
   config => {
